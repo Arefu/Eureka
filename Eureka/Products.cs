1e73488151cf4250e34e5eb8bd4de6d7a789784f
Johnathon <JohnathonKM@GMail.com>
2022-04-25 19:30:12 +1200

    Initial Commit
    Fleet won't work, I don't have a use case for the program so I am not going to invest time in looking into how it works.

---
+++ Eureka/Products.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Eureka
+{
+    static partial class Program
+    {
+        static Products GetProduct()
+        {
+            Console.WriteLine("What Product Do You Want?");
+            Console.WriteLine("1: Basketball");
+            Console.WriteLine("2: Hockey");
+            Console.WriteLine("3: Volleyball");
+            Console.WriteLine("4: Soccer");
+            Console.WriteLine("5: Baseball");
+            Console.WriteLine("6: Softball");
+            Console.WriteLine("7: Wrestling");
+            Console.WriteLine("8: Futsal");
+            Console.WriteLine("9: Signage");
+            Console.WriteLine("10: Safety");
+            Console.WriteLine("11: Fleet");
+            Console.WriteLine("12: POS");
+            int Product = 0;
+            do
+            {
+                var ProductStr = Console.ReadLine();
+                Int32.TryParse(ProductStr, out Product);
+            } while (Product == 0 || Product > 12 || Product < 0);
+
+            switch (Product)
+            {
+                case 1:
+                    return Products.Basketball;
+                case 2:
+                    return Products.Hockey;
+                case 3:
+                    return Products.Handball;
+                case 4:
+                    return Products.Volleyball;
+                case 5:
+                    return Products.Soccer;
+                case 6:
+                    return Products.Baseball;
+                case 7:
+                    return Products.Softball;
+                case 8:
+                    return Products.Wrestling;
+                case 9:
+                    return Products.Futsal;
+                case 10:
+                    return Products.Signage;
+                case 11:
+                    return Products.Safety;
+                case 12:
+                    return Products.Fleet;
+                case 13:
+                    return Products.POS;
+            }
+
+            return Products.Baseball;
+        }
+        enum Products
+        {
+            Basketball = 2001,
+            Hockey = 1918,
+            Handball = 1299,
+            Volleyball = 1951,
+            Soccer = 3866,
+            Baseball = 3851,
+            Softball = 3851,
+            Wrestling = 6,
+            Futsal = 87319,
+            Signage = 87319,
+            Safety = 87319,
+            Fleet = 0,
+            POS = 49183,
+        }
+    }
+}
---
+++ Eureka/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Win32;
+
+namespace Eureka
+{
+    public partial class Program
+    {
+        static void Main()
+        {
+            var PreKey = "";
+            var Registry = (!Environment.Is64BitOperatingSystem) ? RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32) : RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+            var ProductID = Registry.OpenSubKey("Software\\Microsoft\\Windows NT\\CurrentVersion").GetValue("ProductId").ToString();
+            for (int x = 0; x < ProductID.Length; x++)
+            {
+                if (char.IsNumber(ProductID[x]))
+                    PreKey += ProductID[x];
+            }
+
+            for (int x = 0; x < PreKey.Length; x++)
+            {
+                if (PreKey.Substring(x, 1) != "0")
+                {
+                    PreKey = PreKey.Substring(x, PreKey.Length - x);
+                    break;
+                }
+            }
+
+            PreKey = PreKey.Substring(0, 6); //
+
+            var Product = GetProduct();
+            var Key = ((Convert.ToInt32(PreKey) + Convert.ToInt32(Product)) * 2 + Convert.ToInt32(Product)).ToString().PadLeft(10, '0');
+            Console.WriteLine($"Key: {Key}");
+        }
+    }
+}
